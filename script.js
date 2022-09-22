40eca2a85e3abe559534f45236993a205988d00b
alvyynm <alvyynm@gmail.com>
2022-09-22 22:22:14 +0300

    ✨ Make tabbed component work

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const header = document.querySelector(".header");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
 const navLinksContainer = document.querySelector(".nav__links");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabContent = document.querySelectorAll(".operations__content");
 
 // Modal window
 
@@ -69,4 +72,32 @@ navLinksContainer.addEventListener("click", function (e) {
     const id = e.target.getAttribute("href");
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
+});
+
+//Tabbed component
+
+tabsContainer.addEventListener("click", (e) => {
+  const clicked = e.target.closest(".operations__tab");
+
+  // Guard clause
+  if (!clicked) return;
+
+  //Remove active classes
+
+  tabs.forEach(function (t) {
+    t.classList.remove("operations__tab--active");
+  });
+
+  tabContent.forEach((content) =>
+    content.classList.remove("operations__content--active")
+  );
+
+  // Active tab
+  clicked.classList.add("operations__tab--active");
+
+  // Activate content
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
 });
